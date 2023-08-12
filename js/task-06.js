5a273e4bd6859b3284dd5372df1b48c83590fb8f
BielovaIryna <belova83@gmail.com>
2023-08-12 22:56:19 +0300

    hw_fixed

--- js/task-06.js
+++ js/task-06.js
@@ -1,8 +1,8 @@
 const inputEl =document.querySelector("#validation-input");
+const value = parseInt(inputEl.dataset.length);
+
 const check =() =>{
-	if(inputEl.value.length === parseInt(input.dataset.length)){
-		return inputEl.classList.add("valid");
-	}
-	inputEl.classList.add("invalid")
+	
+	inputEl.value.length===value ? inputEl.classList.add('valid') : inputEl.classList.add('invalid'); 
 }
-input.addEventListener("blur", check);
+inputEl.addEventListener("blur", check);
--- js/task-10.js
+++ js/task-10.js
@@ -5,3 +5,30 @@ function getRandomHexColor() {
 }
 
 
+const number = document.querySelector("input");
+const boxes = document.querySelector("#boxes");
+const create = document.querySelector("button[data-create]");
+const destroy=document.querySelector("button[data-destroy]");
+let amm;
+const createBoxes =(amount)=>{
+  for (let index = 0; index <= amount; index+=1) {
+    boxes.insertAdjacentHTML("afterbegin", `<div style="height:20px; width:20px; background-color:red "></div>`)
+  }
+  
+}
+
+
+const handleInput =(event)=>{
+	console.log(amm = event.currentTarget.value)  ;
+}
+
+number.addEventListener("input", handleInput);
+
+console.log(amm);
+
+
+// boxes.insertAdjacentHTML("afterbegin", `<div style="height:20px; width:20px; background-color:red "></div>`)
+
+
+
+
