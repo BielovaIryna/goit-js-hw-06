8dbfce85b45b8a614d1176bb8b647013882548c4
BielovaIryna <belova83@gmail.com>
2023-08-13 23:38:33 +0300

    HW_fixed

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,11 @@ const inputEl =document.querySelector("#validation-input");
 const value = parseInt(inputEl.dataset.length);
 
 const check =() =>{
-	
-	inputEl.value.length===value ? inputEl.classList.add('valid') : inputEl.classList.add('invalid'); 
+	if(inputEl.value.length===value){
+		inputEl.classList.remove('invalid')
+		inputEl.classList.add('valid');
+	} else{
+	inputEl.classList.remove('valid'); 
+	 inputEl.classList.add('invalid');}
 }
 inputEl.addEventListener("blur", check);
